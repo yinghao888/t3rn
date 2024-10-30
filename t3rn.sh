496f2a12c3f8a25973b20d983eb1cc793e88ed94
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-10-30 15:38:06 +0800

    Update t3rn.sh

--- t3rn.sh
+++ t3rn.sh
@@ -97,8 +97,8 @@ function restart_node() {
 # 执行脚本函数
 function execute_script() {
     # 下载文件
-    echo "正在下载 executor-linux-v0.21.10.tar.gz..."
-    wget https://github.com/t3rn/executor-release/releases/download/v0.21.10/executor-linux-v0.21.10.tar.gz
+    echo "正在下载 executor-linux-v0.21.11.tar.gz..."
+    wget https://github.com/t3rn/executor-release/releases/download/v0.21.11/executor-linux-v0.21.11.tar.gz
 
     # 检查下载是否成功
     if [ $? -eq 0 ]; then
@@ -110,7 +110,7 @@ function execute_script() {
 
     # 解压文件到当前目录
     echo "正在解压文件..."
-    tar -xvzf executor-linux-v0.21.10.tar.gz
+    tar -xvzf executor-linux-v0.21.11.tar.gz
 
     # 检查解压是否成功
     if [ $? -eq 0 ]; then
