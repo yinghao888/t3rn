69c26eaf57791480103b85d806fecc19f21047bc
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-12-24 12:53:30 +0800

    Update t3rn.sh

--- t3rn.sh
+++ t3rn.sh
@@ -97,8 +97,8 @@ function restart_node() {
 # 执行脚本函数
 function execute_script() {
     # 下载文件
-    echo "正在下载 executor-linux-v0.22.0.tar.gz..."
-    wget https://github.com/t3rn/executor-release/releases/download/v0.27.0/executor-linux-v0.27.0.tar.gz
+    echo "正在下载 executor-linux-v0.31.0.tar.gz..."
+    wget https://github.com/t3rn/executor-release/releases/download/v0.27.0/executor-linux-v0.31.0.tar.gz
 
     # 检查下载是否成功
     if [ $? -eq 0 ]; then
@@ -110,7 +110,7 @@ function execute_script() {
 
     # 解压文件到当前目录
     echo "正在解压文件..."
-    tar -xvzf executor-linux-v0.27.0.tar.gz
+    tar -xvzf executor-linux-v0.31.0.tar.gz
 
     # 检查解压是否成功
     if [ $? -eq 0 ]; then
@@ -148,7 +148,7 @@ function execute_script() {
 
     # 删除压缩文件
     echo "删除压缩包..."
-    rm executor-linux-v0.27.0.tar.gz
+    rm executor-linux-v0.31.0.tar.gz
 
     # 切换目录并执行脚本
     echo "切换目录并执行 ./executor..."
